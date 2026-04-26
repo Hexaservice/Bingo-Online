8555c273d35ca10765eb7f31d619ed511b4938b8
Hexaservice <hexaservice.co@gmail.com>
2026-04-26 17:08:03 -0400

    Change Firebase config for development setup
    Updated Firebase configuration for development environment.

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,11 +2,11 @@
 // de los marcadores antes de desplegar.
 // Configuración validada para el proyecto bingo-online-231fd.
 window.__FIREBASE_CONFIG__ = {
-  apiKey: "AIzaSyBztIl46s-vOOxrUVDilJNSN6zuzldeUWI",
-  authDomain: "bingo-online-231fd.firebaseapp.com",
-  databaseURL: "https://bingo-online-231fd-default-rtdb.firebaseio.com",
-  projectId: "bingo-online-231fd",
-  storageBucket: "bingo-online-231fd.firebasestorage.app",
-  messagingSenderId: "455917034653",
-  appId: "1:455917034653:web:ef3f7a1d14be86a1580874"
+  apiKey: "AIzaSyDOxWnEYU4WHaHGrn-ncpg4RxjZFu72mXE",
+  authDomain: "bingo-online-dev.firebaseapp.com",
+  databaseURL: "", // si no usas Realtime DB, puedes dejarlo vacío
+  projectId: "bingo-online-dev",
+  storageBucket: "bingo-online-dev.firebasestorage.app",
+  messagingSenderId: "671201853237",
+  appId: "1:671201853237:web:efa00069ae9a39c55d567c"
 };
